ef773448aaa8b260595b5bc8be06765bd7dc3cb4
“Elesheva” <liza0802pos@gmail.com>
2025-09-06 16:16:11 +0300

    Project_two

---
+++ Project_two.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+from sklearn import tree
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from IPython.display import SVG
+from graphviz import Source
+from IPython.display import display
+from IPython.display import HTML
+
+
+
+#Дерево решений
+data = pd.DataFrame({'X_1': [1, 1, 1, 0, 0, 0, 0, 1], 'X_2': [0, 0, 0, 1, 0, 0, 0, 1], 'Y': [1, 1, 1, 1, 0, 0, 0, 0]})
+clf = tree.DecisionTreeClassifier(criterion = "entropy")
+X = data[["X_1", "X_2"]]
+y = data.Y
+clf.fit(X, y)
+tree.plot_tree(clf, feature_names=list(X),
+               class_names=['Negative', 'Positive'],
+               filled=True)
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
